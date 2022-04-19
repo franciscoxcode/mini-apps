fd73e85827cad84390d9936748f64b1d7e5f1a5e
Francisco J. Casillas <chiccasillas@gmail.com>
2022-04-18 20:04:48 -0500

    Use new layout on ADN mini app

--- apps/DNAtoRNA/js/main.js
+++ apps/DNAtoRNA/js/main.js
@@ -1,7 +1,7 @@
-document.querySelector('.convert').addEventListener('click', dnaToRna)
+document.querySelector('.converter').addEventListener('click', dnaToRna)
 
 function dnaToRna() {
     let dna = document.querySelector('#input').value
     let rna = dna.replace(/[T]/gi, "U");
-    document.querySelector('.solution').innerText = rna
+    document.querySelector('.answer').innerText = rna
 }
