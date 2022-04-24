914840815d80822b76a4860e5ad18f08e6d6885c
Francisco J. Casillas <chiccasillas@gmail.com>
2022-04-24 14:59:46 -0500

    Add upper to lower case and vice versa mini app

--- template copy/js/main.js
+++
@@ -1,7 +0,0 @@
-function XO(str){
-    let xs = str.toLowerCase().split('').filter(x => x === 'x').length
-    let os = str.toLowerCase().split('').filter(o => o === 'o').length
-    console.log(xs === os ? true : false)
-    }
-
-XO('xXoOxO')
