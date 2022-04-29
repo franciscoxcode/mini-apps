6d773c5fcd55fe451f9c58b5b27e964a85c2edaf
Francisco J. Casillas <chiccasillas@gmail.com>
2022-04-29 12:00:17 -0500

    Exercise where you can calculate the grade of students

--- exercise/js/main.js
+++ exercise/js/main.js
@@ -1,6 +1,14 @@
-function filter_list(l) {
-	const justNumbers = l.filter((element) => typeof element === "number");
-	console.log(justNumbers);
+function getGrade(s1, s2, s3) {
+	let numericalScore = (s1 + s2 + s3) / 3;
+	if (numericalScore >= 90) {
+		return "A";
+	} else if (numericalScore >= 80) {
+		return "B";
+	} else if (numericalScore >= 70) {
+		return "C";
+	} else if (numericalScore >= 60) {
+		return "D";
+	} else if (numericalScore >= 0) {
+		return "F";
+	}
 }
-
-filter_list([1, 2, "aasf", "1", "123", 123]);
