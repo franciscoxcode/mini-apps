b29800bf2951b2b287b75bf741d6c742661b8c1e
Francisco J. Casillas <chiccasillas@gmail.com>
2022-04-30 11:09:40 -0500

    Exercise to filter pin digits

--- exercise/js/main.js
+++ exercise/js/main.js
@@ -1,14 +1,13 @@
-function getGrade(s1, s2, s3) {
-	let numericalScore = (s1 + s2 + s3) / 3;
-	if (numericalScore >= 90) {
-		return "A";
-	} else if (numericalScore >= 80) {
-		return "B";
-	} else if (numericalScore >= 70) {
-		return "C";
-	} else if (numericalScore >= 60) {
-		return "D";
-	} else if (numericalScore >= 0) {
-		return "F";
+function validatePIN(pin) {
+	if (pin.length === 4 || pin.length === 6) {
+		if (Number.isInteger(Number(pin)) === true && Number(pin) >= 0) {
+			console.log(true);
+		} else {
+			console.log(false);
+		}
+	} else {
+		console.log(false);
 	}
 }
+
+validatePIN("-12345");
